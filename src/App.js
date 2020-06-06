8efc4769d549218d98630573b0c369f7ec72da75
Kevin Matthews <kevmatthews3@gmail.com>
2020-06-05 21:53:52 -0400

    set-components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
+import { AddTransaction } from "./components/AddTransaction";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Expense Tracker</p>
-      </header>
+    <div>
+      <Header />
+
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
     </div>
   );
 }
